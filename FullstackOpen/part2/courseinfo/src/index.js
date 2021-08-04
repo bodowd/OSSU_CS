f755bc6dfd9847fa801a3ba85f3603551b775b81
Bing <odowd.bing@gmail.com>
2021-08-04 15:23:32 +0200

    2.1 course information step 6

---
+++ FullstackOpen/part2/courseinfo/src/index.js
@@ -0,0 +1,71 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
+};
+
+const Total = ({ course }) => {
+  const sum = course.parts.reduce((acc, v) => acc = acc + v.exercises, 0)
+  return <p>Number of exercises {sum}</p>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.ex}
+    </p>
+  );
+};
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map(p => <Part key={p.id} name={p.name} ex={p.exercises} />)}
+    </div>
+  );
+};
+
+const Course = (props) => {
+  return (
+    <div>
+      <Header course={props.course} />
+      <Content course={props.course} />
+      <Total course={props.course} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+      // test that it works on more data
+      {
+        name: "State of a component test-second part",
+        exercises: 14,
+        id: 4,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
