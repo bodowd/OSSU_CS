330c7e8497474dd6b2ed3bdb92e17781934e8278
Bing <odowd.bing@gmail.com>
2021-08-05 13:38:58 +0200

    2.6 phonebook

---
+++ FullstackOpen/part2/phonebook/src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+const Person = ({person}) => {
+  return (
+    <div>{person.name}</div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName
+    }
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+    console.log(persons);
+  }
+
+  const handleNameChange = (event) => {
+    // console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+        <div>
+          {persons.map(person => <Person person={person}/> )}
+        </div>
+    </div>
+  );
+};
+
+export default App;
