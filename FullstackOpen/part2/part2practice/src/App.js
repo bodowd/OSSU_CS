ee674a88d5083434a3db41f9bcc841d5e559049f
bodowd <odowd.bing@gmail.com>
2021-08-14 21:32:11 +0200

    improved error message

--- FullstackOpen/part2/part2practice/src/App.js
+++ FullstackOpen/part2/part2practice/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Note from './components/Note'
+import Notification from './components/Notification'
 // we don't define `noteService` explicity. we just name what is exported from notes service as noteService
 import noteService from './services/notes'
 
@@ -8,6 +9,7 @@ const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
 
 
   const hook = () => {
@@ -68,9 +70,12 @@ const App = () => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
       .catch(error => {
-        alert(
-          `the note '${note.content}' was already deleted from server`
+        setErrorMessage(
+          `Note '${note.content}' was already removed from the server`
         )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
   }
@@ -78,6 +83,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
