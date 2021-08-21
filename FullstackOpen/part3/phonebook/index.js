b36c6cc698b9a1c6d0c6d125437811cbd9dfdbd1
bodowd <odowd.bing@gmail.com>
2021-08-21 14:56:46 +0200

    connected to backend and made build

--- FullstackOpen/part3/phonebook/index.js
+++ FullstackOpen/part3/phonebook/index.js
@@ -2,10 +2,12 @@ const { response } = require('express')
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
-
+app.use(cors())
 // THIS IS SUPER IMPORTANT. initial handler for dealing with HTTP POST requests
 app.use(express.json())
+app.use(express.static('build'))
 
 morgan.token('body', function (req, res) {return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
