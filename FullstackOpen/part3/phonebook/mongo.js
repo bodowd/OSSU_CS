aeb9f1aad0d2777a933c387fa2387a15d22e0ebf
bodowd <odowd.bing@gmail.com>
2021-08-23 22:29:06 +0200

    exercise 3.12

---
+++ FullstackOpen/part3/phonebook/mongo.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.g4r6q.mongodb.net/test?retryWrites=true&w=majority`
+
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+})
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Persons = mongoose.model('Persons', personSchema)
+
+// if only node mongo.js <password> is invoked, just print out all entries of the phonebook
+if (process.argv.length === 3) {
+    Persons.find({}).then(persons => {
+        console.log('phonebook:')
+        persons.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+} else {
+
+    const person = new Persons({
+        name: process.argv[3],
+        number: process.argv[4]
+    })
+
+    person.save().then(result => {
+        console.log(`added ${person.name} number ${person.number} to phonebook`)
+        mongoose.connection.close()
+    })
+
+}
+
+
