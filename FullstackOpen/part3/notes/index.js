cffe10b703c990262407a0cdb4b3fea17b64f947
bodowd <odowd.bing@gmail.com>
2021-08-25 07:46:51 +0200

    added delete and update functions

--- FullstackOpen/part3/notes/index.js
+++ FullstackOpen/part3/notes/index.js
@@ -9,11 +9,24 @@ const Note = require('./models/note')
 
 
 
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
-app.use(express.static('build'))
 
 // Middleware is a function that receives three parameters
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
 // the next function yields control to the next middleware
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -25,64 +38,7 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger)
 
-// const url = `mongodb+srv://fullstack:${process.env.NOTES_DB_PASS}@cluster0.uuykr.mongodb.net/note-app?retryWrites=true&w=majority`
-
-// mongoose.connect(url, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false,
-//     useCreateIndex: true
-// })
-
-// const noteSchema = new mongoose.Schema({
-//     content: String,
-//     date: Date,
-//     important: Boolean
-// })
-
-// noteSchema.set('toJSON', {
-//     transform: (document, returnedObject) => {
-//         // the _id property looks like a string, but is in fact an object. Transform it to a string to be safe once we start writing tests
-//         returnedObject.id = returnedObject._id.toString()
-//         delete returnedObject._id
-//         delete returnedObject.__v
-//     }
-// })
-
-// const Note = mongoose.model('Note', noteSchema)
-
-// let notes = [{
-//         id: 1,
-//         content: "HTML is easy",
-//         date: "2019-05-30T17:30:31.098Z",
-//         important: true
-//     },
-//     {
-//         id: 2,
-//         content: "Browser can execute only Javascript",
-//         date: "2019-05-30T18:39:34.091Z",
-//         important: false
-//     },
-//     {
-//         id: 3,
-//         content: "GET and POST are the most important methods of HTTP protocol",
-//         date: "2019-05-30T19:20:14.298Z",
-//         important: true
-//     },
-//     {
-//         id:4,
-//         content: "New note",
-//         date: "2021-05-01",
-//         important: true
-//     },
-//     {
-//         id:5,
-//         content: "Another one",
-//         date: "2021-02-20",
-//         important: false
-//     }
-// ]
-
+// -------- ROUTES ----------
 app.get('/', (request, response) => {
     response.send('<h1>Hello world</h1>')
 })
@@ -106,14 +62,14 @@ const generateId = () => {
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
-    
+
     if (!body.content) {
         // if received data is missing a value for the content property, respond with 400
         return response.status(400).json({
             error: 'content missing'
         })
     }
-    
+
     const note = new Note({
         content: body.content,
         important: body.important || false,
@@ -126,26 +82,59 @@ app.post('/api/notes', (request, response) => {
     })
 })
 
+
+
 // define paramters for routes in ExpressJS by using the colon syntax
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     // the id parameter in the route of a request can be accessed through the request object
     // id variable is a string whereas the id of notes are integers. need to change to int to do comparison
-    Note.findById(request.params.id).then(note => {
-        response.json(note)
-    })
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note) {
+                response.json(note)
+            } else {
+                response.status(404).end()
+            }
+        })
+        // if malformed invalid mongodb id is given as argument, the returned promise will be rejected and cause the catch block to be called
+        // if next was called without a parameter, the execution would simply move onto the next route or middleware. If the next function is called with a parameter,
+        // then the execution will continue to the error handler middleware
+        .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-    // if deleting the resource is successful, meaning that the note exists and it is removed, respond with 204 no content status code
-    response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndRemove(request.params.id)
+        // if deleting the resource is successful, meaning that the note exists and it is removed, respond with 204 no content status code
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({error: 'unknown endpoint'})
-}
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+    const note = {
+        content: body.content,
+        important: body.important
+    }
+
+    // mongodb model function: {new: true} option returns the modified document rather than the original. defaults to false
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
+})
+
+
+
+// THESE MIDDLEWARE NEED TO BE HERE TOWARD THE END IN THIS ORDER...
+// handler of requests with unkown endpoint. This responds to all requests with 404 unknown endpoint, and no routes or middleware will be called after this response has been sent by unknown endpoint middleware
+// if this was ordered before app.get('/api/notes'...) since this responds to all requests with a 404 unknown endpoint, no routes or middleware will be called 
+// after the response has been sent by unknown endpoint middleware. Everything would return with unknown endpoint, except for the error handler which comes after the endpoints handler
 app.use(unknownEndpoint)
+// this has to be the last loaded middleware 
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 // need to write it with localhost specified to make VSCode Rest Client extension work
