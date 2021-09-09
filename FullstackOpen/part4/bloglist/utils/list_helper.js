a991c5570927e22b18eeab08b0a70973c777b4bf
bodowd <odowd.bing@gmail.com>
2021-09-09 08:19:54 +0200

    4.5 bloglist tests

--- FullstackOpen/part4/bloglist/utils/list_helper.js
+++ FullstackOpen/part4/bloglist/utils/list_helper.js
@@ -13,7 +13,23 @@ const totalLikes = (blogs) => {
         : blogs.reduce(reducer,0)
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return null
+    }
+    // reduce(previousValue, currentValue)
+    // previousValue will be x or y depending on the ternary 
+    // currentValue starts at blogs[0], but will be replaced by previous value
+    const result = blogs.reduce((x, y) => x.likes > y.likes ? x : y, blogs[0])
+    return {
+        title: result.title,
+        author: result.author,
+        likes: result.likes
+    }
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
