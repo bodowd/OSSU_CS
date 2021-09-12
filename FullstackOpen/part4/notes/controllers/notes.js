625941650a9951e724869f72a1f0f3042b8ed5d4
bodowd <odowd.bing@gmail.com>
2021-09-12 22:06:03 +0200

    up to eliminating the try-catch

--- FullstackOpen/part4/notes/controllers/notes.js
+++ FullstackOpen/part4/notes/controllers/notes.js
@@ -7,27 +7,27 @@ const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
 // -------------- GET routes -----------
-notesRouter.get('/', (request, response) => {
-    Note.find({}).then(notes => {
-        response.json(notes)
-    })
+notesRouter.get('/', async (requet, response) => {
+    const notes = await Note.find({})
+    response.json(notes)
 })
 
 // the notesRouter object must only define the relative parts of the routes, i.e. the empty path / or just the parameter /:id
-notesRouter.get('/:id', (request, response, next) => {
-    Note.findById(request.params.id)
-        .then(note => {
-            if (note) {
-                response.json(note)
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+notesRouter.get('/:id', async (request, response, next) => {
+    try {
+        const note = await Note.findById(request.params.id)
+        if (note) {
+            response.json(note)
+        } else {
+            response.status(404).end()
+        }
+    } catch(exception) {
+        next(exception)
+    }
 })
 
 // --------------- POST routes ------------
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response, next) => {
     const body = request.body
 
     const note = new Note({
@@ -35,37 +35,38 @@ notesRouter.post('/', (request, response, next) => {
         important: body.important || false,
         date: new Date()
     })
-
-    note.save()
-        .then(savedNote => {
-            response.json(savedNote)
-        })
-        .catch(error => next(error))
+    try {
+        const savedNote = await note.save()
+        response.json(savedNote)
+    } catch(exception) {
+        next(exception)
+    }
 })
 
 // ------------ DELETE routes -----------
-notesRouter.delete('/:id', (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+notesRouter.delete('/:id', async (request, response, next) => {
+    try {
+        await Note.findByIdAndRemove(request.params.id)
+        response.status(204).end()
+    } catch(exception) {
+        next(exception)
+    }
 })
 
 // ----------- PUT routes ------------
 notesRouter.put('/:id', (request, response, next) => {
-  const body = request.body
+    const body = request.body
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
+    const note = {
+        content: body.content,
+        important: body.important,
+    }
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then(updatedNote => {
-      response.json(updatedNote)
-    })
-    .catch(error => next(error))
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
 })
 
 
--- FullstackOpen/part4/notes/utils/logger.js
+++ FullstackOpen/part4/notes/utils/logger.js
@@ -1,11 +1,15 @@
 // info for printing normal log messages
 const info = (...params) => {
-    console.log(...params)
+    if (process.env.NODE_ENV !== 'test') {
+        console.log(...params)
+    }
 }
 
 // error for all error messages
 const error = (...params) => {
-    console.error(...params)
+    if (process.env.NODE_ENV !== 'test') {
+        console.error(...params)
+    }
 }
 
 module.exports = {
