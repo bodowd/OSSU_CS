7ecece1ef2bb5e57b2aa5423fe2888e13a4e3def
bodowd <odowd.bing@gmail.com>
2021-09-26 22:33:16 +0200

    4.15 bloglist

--- FullstackOpen/part4/bloglist/tests/blog_api.test.js
+++ FullstackOpen/part4/bloglist/tests/blog_api.test.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 const helper = require('./test_helper')
 
 const api = supertest(app)
@@ -126,6 +127,39 @@ describe('update of a blog', () => {
         const edited = blogsAtEnd.find(b => b.id === blogToUpdate.id)
         expect(edited.likes).toBe(10)
 
+    })
+})
+
+describe('when there is initially one user in db', () => {
+    beforeEach(async () => {
+        await User.deleteMany({})
+
+        const passwordHash = await bcrypt.hash('sekret', 10)
+        const user = new User({ username: 'root', passwordHash })
+
+        await user.save()
+    })
+
+    test('creation succeeds with fresh username', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'mluukkai',
+            name: 'Matti Luukkainen',
+            password: 'salainen'
+        }
+
+        await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+        const usernames = usersAtEnd.map(u => u.username)
+        expect(usernames).toContain(newUser.username)
     })
 })
 
