b061ba705a5cdbbfadcb33ea90d70ee07fa8dd16
bodowd <odowd.bing@gmail.com>
2021-10-02 22:54:49 +0200

    5.2 bloglist frontend

--- FullstackOpen/part5/bloglist_frontend/src/App.js
+++ FullstackOpen/part5/bloglist_frontend/src/App.js
@@ -19,12 +19,23 @@ const App = () => {
       )
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     try {
       const user = await loginService.login({
         username, password
       })
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
+      blogService.setToken(user.token)
 
       setUser(user)
       setUsername('')
@@ -35,6 +46,11 @@ const App = () => {
         setErrorMessage(null)
       }, 5000)
     }
+  }
+
+  const handleLogout = async (event) => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    window.location.reload(false)
   }
 
   const loginForm = () => (
@@ -59,6 +75,7 @@ const App = () => {
      :
       <div>
         <p>{user.name} logged in</p>
+        <button onClick={() => handleLogout()}>logout</button>
         {blogs.map(blog => <Blog key={blog.id} blog={blog}/>)}
       </div>
       }
--- FullstackOpen/part5/bloglist_frontend/src/services/blogs.js
+++ FullstackOpen/part5/bloglist_frontend/src/services/blogs.js
@@ -1,9 +1,15 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+// private variable token to be changed with the setToken function
+let token = null
+const setToken = newToken => {
+    token = `bearer ${newToken}`
+}
+
 const getAll = () => {
     const request = axios.get(baseUrl)
     return request.then(response => response.data)
 }
 
-export default { getAll }
+export default { getAll, setToken }
