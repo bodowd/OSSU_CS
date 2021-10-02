ff5423a76b6c1d953c3c340601baa80c1fe60422
bodowd <odowd.bing@gmail.com>
2021-10-02 17:41:46 +0200

    added conditional rendering

--- FullstackOpen/part5/bloglist_frontend/src/App.js
+++ FullstackOpen/part5/bloglist_frontend/src/App.js
@@ -52,7 +52,11 @@ const App = () => {
       <h2>blogs</h2>
       <Notification message={errorMessage} />
       {user === null ?
-      loginForm() :
+      <div>
+        <h2>Log in to application</h2>
+        {loginForm()}
+      </div>
+     :
       <div>
         <p>{user.name} logged in</p>
         {blogs.map(blog => <Blog key={blog.id} blog={blog}/>)}
