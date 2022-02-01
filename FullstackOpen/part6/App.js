5333775055565c76c72bd4ce9d8346a73fb32606
bodowd <odowd.bing@gmail.com>
2022-02-01 16:54:36 +0100

    up to action creators

--- FullstackOpen/part6/App.js
+++ FullstackOpen/part6/App.js
@@ -3,13 +3,25 @@ import ReactDOM from 'react-dom'
 import { createStore } from 'redux'
 
 const noteReducer = (state = [], action) => {
-  if (action.type === 'NEW_NOTE') {
-    // create a new array with concat instead of modifying in place with push
-    state.concat(action.data)
-    return state
+  switch (action.type) {
+    case 'NEW_NOTE':
+      // return state.concat(action.data)
+      // or using the JS array spread syntax
+      return [...state, action.data]
+    case 'TOGGLE_IMPORTANCE': {
+      const id = action.data.id
+      const noteToChange = state.find((n) => n.id === id)
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important,
+      }
+      // a new state is returned. Created by taking all of the notes from the old state, except for the desired note, which we replace
+      // with its slightly altered copy
+      return state.map((note) => (note.id !== id ? note : changedNote))
+    }
+    default:
+      return state
   }
-
-  return state
 }
 
 const store = createStore(noteReducer)
@@ -32,15 +44,43 @@ store.dispatch({
   },
 })
 
+const generateId = () => Math.floor(Math.random() * 10000000)
+
 const App = () => {
+  const addNote = (event) => {
+    event.preventDefault()
+    // we will name the input field in the html with note. That is how we can get event.target.note here
+    const content = event.target.note.value
+    event.target.note.value = ''
+    store.dispatch({
+      type: 'NEW_NOTE',
+      data: {
+        content,
+        important: false,
+        id: generateId(),
+      },
+    })
+  }
+
+  const toggleImportance = (id) => {
+    store.dispatch({
+      type: 'TOGGLE_IMPORTANCE',
+      data: { id },
+    })
+  }
   return (
     <div>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
       <ul>
-        {store.getState().map((note) => {
-          <li key={note.id}>
+        {store.getState().map((note) => (
+          // change the notes importance by clicking on its name
+          <li key={note.id} onClick={() => toggleImportance(note.id)}>
             {note.content} <strong>{note.important ? 'important' : ''}</strong>
           </li>
-        })}
+        ))}
       </ul>
     </div>
   )
